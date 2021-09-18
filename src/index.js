d9ba1cc0f34a9fbbfbf71d26c4a62210f36ead47
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-09-18 12:07:47 +0200

    Lodash fp higher or functions (compose and pipe) for the win

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+import { compose, pipe } from "lodash/fp";
+
+let input = "       jaVASCript      ";
+
+let output = "<div>" + input.trim() + "</div>";
+
+const trim = (untrimmed) => untrimmed.trim();
+
+const wrap = (unwrapped) => `<div>${unwrapped}</div>`;
+
+const toLowerCase = (string) => string.toLowerCase();
+
+const transform = pipe(trim, toLowerCase, wrap);
+
+const result = transform(input);
+
+console.log(result);
